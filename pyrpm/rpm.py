c7fe4b9f45c90e035d64577d9c21085ee43f93f9
pombredanne <pombredanne@nexb.com>
2015-07-17 11:04:16 +0200

    Smal refinemenst and adding license notice

--- pyrpm/rpm.py
+++ pyrpm/rpm.py
@@ -1,22 +1,45 @@
 # -*- coding: iso-8859-15 -*-
 # -*- Mode: Python; py-ident-offset: 4 -*-
 # vim:ts=4:sw=4:et
+
+# Copyright (c) Mário Morgado
+# 
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# 
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 '''
 PyRPM
 =====
 
 PyRPM is a pure python, simple to use, module to read information from a RPM file.
 
-$Id$
 '''
-__revision__ = '$Rev$'[6:-2]
+from __future__ import absolute_import
 
 
 from StringIO import StringIO
 import struct
 import re
 
-from pyrpm import rpmdefs
+from . import rpmdefs
 
 HEADER_MAGIC_NUMBER = re.compile('(\x8e\xad\xe8)')
 
